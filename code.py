41408bb4221b8eab54089939bd3667c4eef41270
Łukasz Rąbalski <lukaszrabalski@gmail.com>
2019-04-21 15:04:11 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,6 @@
-#Below I present aim of creating a class
+#Below I present two class tasks
+
+#TASK 1
 
 #Create an class which will hold the name of store, and automatically create list of prodcut sold in this store.
 #This class should have two methods:
@@ -40,3 +42,42 @@ Store.store_detail(Biedronka)
 
 zabka_franch=Store.franchise("Zabka")
 print(zabka_franch.name)
+
+
+#TASK 2
+#Create class call Student, which will gather information about student's name and school, and will have a box for his marks
+
+#Then create two methods:
+#1) which will calculate students mark's average
+#2) static one, which will allow for creating a Student profile by adding information about with whom given student is walking to school (suck information about student
+#school)
+
+#Then create new class WorkingStudent which inherit from Student one all its properties but additionally contain information about salary
+
+class Student:
+    def __init__(self, name,school):
+        self.name=name
+        self.school=school
+        self.marks=[]
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+    
+    @classmethod
+    def friend(cls, origin,friend_name, *args):
+        return cls(friend_name,origin.school, *args)
+
+class WorkingStudent(Student):
+    def __init__(self, name, school, salary):
+        super().__init__(name, school)
+        self.salary=salary
+
+anna=WorkingStudent("Anna","Oxford",20.0)
+print(anna.school)
+print(anna.salary)
+Greg=WorkingStudent.friend(anna,"Greg",25)
+print(Greg.school)
+Luk=Student.friend(anna,"Luk")
+print(Luk.school)
+
+
